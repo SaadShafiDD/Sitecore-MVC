cdca195c1e05343e454c2d8e41c17b941a495b62
SaadShafiDD <96863205+SaadShafiDD@users.noreply.github.com>
2023-11-29 12:01:37 +0500

    Item Url with Link Manager

---
+++ Sitecore.Demo.MVC.Web/Extensions/ItemExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+using Sitecore.Links;
+using Sitecore.Links.UrlBuilders;
+
+namespace Sitecore.Demo.MVC.Web.Extensions
+{
+    public static class ItemExtension
+    {
+        public static string GetItemUrl(this Item item, ItemUrlBuilderOptions options=null)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(Item));
+            else if (options != null)
+                return LinkManager.GetItemUrl(item, options);
+            else
+                return LinkManager.GetItemUrl(item);
+        }
+    }
+}
